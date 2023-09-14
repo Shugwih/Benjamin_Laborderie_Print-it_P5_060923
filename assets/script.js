d819f2819b09da0b4515d625c61699d9b5df3498
Benjamin Laborderie <benjamin.laborderie@gmail.com>
2023-09-14 18:20:27 +0200

    Add alpha 0.2

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,3 @@
-/**const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]**/
-/**
-	arrow = document.querySelectorAll(".arrow")
-
-	arrow.forEach(img => {
-		img.addEventListener("click", () => {
-			console.log(img)
-		})
-	})
-**/ /**
-const slides = [
-	"slide1.jpg", //Index0
-	"slide2.jpg", //Index1
-	"slide3.jpg", //Index2
-	"slide4.png" //Index3
-]
-
-let currentIndex = 0;
-
-function showSlide () {
-	let currentSlide = slides[currentIndex] 
-
-	console.log(currentSlide)
-}
-
-showSlide ()
-
-function changeSlide (newIndex) {
-	currentIndex += newIndex;
-	showSlide ();
-	console.log({newIndex, currentIndex})
-}**/
-
 //SLIDE AND BANNER
 const slides = [
     {
@@ -68,25 +18,23 @@ const slides = [
       tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
     },
 ], 
-  	  bannerImg = document.querySelector(".banner-img"),
-	    bannerTxt = document.querySelector("#banner p");
+  	bannerImg = document.querySelector(".banner-img"),
+	bannerTxt = document.querySelector("#banner p");
 
 //ARROW
 const leftArrow = document.querySelector(".arrow_left"),
-	    rightArrow = document.querySelector(".arrow_right"),
-	    arrow = document.querySelectorAll(".arrow"); //ATTENTION TODO
+	rightArrow = document.querySelector(".arrow_right"),
+	arrow = document.querySelectorAll(".arrow"); //ATTENTION TODO
 
 //SLIDES ELEMENTS
 const slideElement = slides.length -1, // index 0 départ + -1 parcequ'il reste 3 img et pas 4
-  	  imgSrc = "./assets/images/slideshow/";
+  	imgSrc = "./assets/images/slideshow/";
 
 let currentIndex = 0; // point de départ
 
 //DOT
 const containerDots = document.querySelector(".dots"),
-      eachDot = `<span class="dot"></span>`,
-      allDot = document.querySelectorAll(".dot");
-
+    eachDot = `<span class="dot"></span>`;
 
 function activeSlide (select) { //select???
 	bannerImg.src = imgSrc + slides[select]["image"]; //imgSrc devant pour avoir le bon path
@@ -114,14 +62,23 @@ leftArrow.addEventListener("click", () => {
 function dotFunction () {
   for (let sliderDot = 0; sliderDot <= slideElement; sliderDot++) {
     containerDots.innerHTML += eachDot;
-    const dotSelect = () => {
-      if (sliderDot === currentIndex) {
-        allDot.classList.add("dot_selected");
-      } else {
-        allDot.classList.remove("dot_selected");
-      }
-    }
-	console.log(allDot)
+	console.log(sliderDot)
   }
 };
 dotFunction ()
+
+dot = document.querySelector(".dot");
+dot.addEventListener("click", () => {
+	console.log(1)
+})
+
+function dotSelect () {
+	for (let sliderDot = 0; sliderDot <= slideElement; sliderDot++) {
+		if (sliderDot === slideElement) {
+			dot.classList.add("dot_selected");
+		} else {
+			dot.classList.remove("dot_selected");
+		}
+	}
+};
+dotSelect()
